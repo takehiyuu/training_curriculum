40e6ce45f2d9953692901985011d39a5bab63317
takehiyuu <77262296+takehiyuu@users.noreply.github.com>
2021-02-01 18:48:16 +0900

    wday条件分岐の演算子修正

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -28,15 +28,16 @@ class CalendarsController < ApplicationController
     @week_days = []
 
     plans = Plan.where(date: @todays_date..@todays_date + 6)
-
+    
     7.times do |x|
       today_plans = []
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
 
-      wday_num = @todays_date.wday
-      if wday_num > 7
+      wday_num = @todays_date.wday + x
+
+      if wday_num >= 7
         wday_num = wday_num - 7
       end
       days = { month:  (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans, wday: wdays[wday_num]}
